e696959616d846472ad780e4c32a893b59c1a345
dmddjack <dmddjack@gmail.com>
2020-08-06 12:13:05 +0800

    code improvement

--- sum_k.py
+++ sum_k.py
@@ -1,20 +1,18 @@
-import random
+def sumk(nums, k):
+    list1 = []
+    count = 0
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == k:
+                list1.append((nums[i], nums[j]))
+                count += 1
+    if count == 0:
+        print('no results')
+    else:
+        for i in range(count):
+            print('{0} + {1} = {2}'.format(list1[i][0], list1[i][1], k))
 
-list1 = []
-count = 0
-list0 = random.choices(range(1, 10), k=random.randint(4, 10))
-k = random.randint(5, 20)
-for i in range(len(list0)):
-    for j in range(i, len(list0)):
-        if i == j:
-            continue
-        if list0[i] + list0[j] == k:
-            list1.append((list0[i], list0[j]))
-            count += 1
-print('selected list = {0}'.format(list0))
-print('k =  {0}'.format(k))
-if count == 0:
-    print('no result')
-else:
-    for i in range(count):
-        print('{0} + {1} = {2}'.format(list1[i][0], list1[i][1], k))
+
+sumk([1, 2, 3, 4], 5)
+print()
+sumk([1, 2, 3, 4], 8)
